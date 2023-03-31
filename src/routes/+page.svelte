1af561661c907a0dfda542eaea00a3b39830389f
adriano <slipkaze123@gmail.com>
2023-03-31 13:38:08 -0300

    first??

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div class="p-4 h-screen flex justify-center items-center">
+    <div class="border border-neutral-200 shadow-sm p-3" >
+         <input type="checkbox"/>
+        <span>essa é minha tarefa legal</span>
+    
+    </div>
+    <button>clica aqui kk</button>
+</div>
+<task>
+
+</task>
